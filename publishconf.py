a3cf20f8f172c486998a5f2f2614b89c56a9fad8
Talha Mansoor <talha131@gmail.com>
2013-08-24 22:57:30 +0500

    Add robots.txt to the output submodule

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,6 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = u'http://oncrashreboot.com'
-OUTPUT_RETENTION = ['.git', '.gitignore', 'CNAME', 'README.md']
+OUTPUT_RETENTION = ['.git', '.gitignore', 'CNAME', 'README.md', 'robots.txt']
 DELETE_OUTPUT_DIRECTORY = True
 GOOGLE_ANALYTICS = u'UA-43454971-1'
